d8efe184ba9c46e6ebe93480ac5b9a89aca70a97
govind <govindkumarsam@gmail.com>
2023-09-03 17:45:46 +0530

    editing from the ui

--- src/App.js
+++ src/App.js
@@ -5,8 +5,28 @@ import React, { useState } from "react";
 import { nanoid } from "nanoid";
 function App(props) {
   const [tasks, setTasks] = useState(props.tasks);
-  function toggleTaskCompleted(id){
-    console.log(tasks[0]);
+  function toggleTaskCompleted(id) {
+    const updatedTasks = tasks.map((task) => {
+      if (id === task.id) {
+        return { ...task, completed: !task.completed };
+      }
+      return task;
+    });
+    setTasks(updatedTasks);
+  }
+  function deleteTask(id){
+    const remainingTasks = tasks.filter((task) => id !==task.id);
+    setTasks(remainingTasks);
+    console.log(id);
+  }
+  function editTask(id, newName){
+    const editedTaskList = tasks.map((task) =>{
+      if(id === task.id){
+        return{...task, name: newName};
+      }
+      return task;
+    });
+    setTasks(editedTaskList);
   }
   const taskList = tasks.map((task) => (
     <Todo
@@ -14,26 +34,26 @@ function App(props) {
       name={task.name}
       completed={task.completed}
       key={task.id}
-      toggleTaskCompleted = {toggleTaskCompleted}
+      toggleTaskCompleted={toggleTaskCompleted}
+      deleteTask ={deleteTask}
+      editTask = {editTask}
     />
   ));
-  const tasksNoun = taskList.length !== 1 ? "tasks" : "task";
-
+  const tasksNoun = taskList.length > 1 ? "tasks" : "task";
   const headingText = `${taskList.length} ${tasksNoun} remaining`;
-
-  function addTask(name){
-    const newTask = {id: `todo-${nanoid}`, name, completed: false}
+  function addTask(name) {
+    const newTask = { id: `todo-${nanoid()}`, name, completed: false }
     setTasks([...tasks, newTask]);
   }
   
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
-      <Form addTask = {addTask}/>
+      <Form addTask={addTask} />
       <div className="filters btn-group stack-exception">
-      <FilterButton filter = "All"/>
-      <FilterButton filter = "Active"/>
-      <FilterButton filter = "Completed"/>
+        <FilterButton filter="All" />
+        <FilterButton filter="Active" />
+        <FilterButton filter="Completed" />
       </div>
       <h2 id="list-heading">{headingText}</h2>
       <ul
